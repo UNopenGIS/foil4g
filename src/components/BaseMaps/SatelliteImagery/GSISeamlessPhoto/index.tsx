880eb678232234cb29ecddb2cf86c01a639ae049
yuiseki <yuiseki@gmail.com>
2024-06-29 13:52:09 +0900

    Add GIS SeamlessPhoto Preview

---
+++ src/components/BaseMaps/SatelliteImagery/GSISeamlessPhoto/index.tsx
@@ -0,0 +1,19 @@
+import { Map } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export const GSISeamlessPhoto = () => {
+  return (
+    <Map
+      initialViewState={{
+        longitude: 0,
+        latitude: 0,
+        zoom: 4,
+      }}
+      dragPan={false}
+      scrollZoom={false}
+      hash={false}
+      style={{ width: "100%", height: "100%" }}
+      mapStyle="stylejson/cyberjapandata.gsi.go.jp/xyz/seamlessphoto/style.json"
+    />
+  );
+};
