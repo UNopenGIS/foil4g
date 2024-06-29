4bb4e6008a2410016b980083e4f716d263cb5cfa
yuiseki <yuiseki@gmail.com>
2024-06-29 13:39:48 +0900

    fix style of map

--- src/components/OpenStreetMap/OpenStreetMapJpOSMBrightVector/index.tsx
+++ src/components/OpenStreetMap/OpenStreetMapJpOSMBrightVector/index.tsx
@@ -3,22 +3,17 @@ import "maplibre-gl/dist/maplibre-gl.css";
 
 export const OpenStreetMapJpOSMBrightVector = () => {
   return (
-    <div>
-      <h2>
-        https://tile.openstreetmap.jp/styles/osm-bright/style.json
-      </h2>
-      <Map
-        initialViewState={{
-          longitude: 0,
-          latitude: 0,
-          zoom: 4,
-        }}
-        dragPan={false}
-        scrollZoom={false}
-        hash={false}
-        style={{ width: "100%", height: "400px" }}
-        mapStyle="https://tile.openstreetmap.jp/styles/osm-bright/style.json"
-      />
-    </div>
+    <Map
+      initialViewState={{
+        longitude: 0,
+        latitude: 0,
+        zoom: 4,
+      }}
+      dragPan={false}
+      scrollZoom={false}
+      hash={false}
+      style={{ width: "100%", height: "100%" }}
+      mapStyle="https://tile.openstreetmap.jp/styles/osm-bright/style.json"
+    />
   );
 };
--- src/components/UNClearMapRaster/index.tsx
+++ src/components/UNClearMapRaster/index.tsx
@@ -3,22 +3,17 @@ import "maplibre-gl/dist/maplibre-gl.css";
 
 export const UNClearMapRaster = () => {
   return (
-    <div>
-      <h2>
-        https://unopengis.github.io/foil4g/stylejson/geoservices.un.org/clearmap/style.json
-      </h2>
-      <Map
-        initialViewState={{
-          longitude: 0,
-          latitude: 0,
-          zoom: 4,
-        }}
-        dragPan={false}
-        scrollZoom={false}
-        hash={false}
-        style={{ width: "100%", height: "400px" }}
-        mapStyle="stylejson/geoservices.un.org/clearmap/style.json"
-      />
-    </div>
+    <Map
+      initialViewState={{
+        longitude: 0,
+        latitude: 0,
+        zoom: 4,
+      }}
+      dragPan={false}
+      scrollZoom={false}
+      hash={false}
+      style={{ width: "100%", height: "100%" }}
+      mapStyle="stylejson/geoservices.un.org/clearmap/style.json"
+    />
   );
 };
