33ed0f0ec81375825550872c18538d38a3b30be7
yuiseki <yuiseki@gmail.com>
2024-06-29 15:29:18 +0900

    feat: Add mapStyleOptions for different map styles

--- src/components/Combined/OpenCellId/index.stories.ts
+++ src/components/Combined/OpenCellId/index.stories.ts
@@ -1,19 +1,12 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { OpenCellIdWithStyle } from ".";
+import { mapStyleOptions } from "../../mapStyleOptions";
 
 const meta = {
   component: OpenCellIdWithStyle,
   argTypes: {
     mapStyle: {
-      options: [
-        "stylejson/tile.openstreetmap.jp/fiord-color-gl-style/style.json",
-        "stylejson/tile.openstreetmap.fr/hot/style.json",
-        "stylejson/tile.openstreetmap.org/style.json",
-        "https://tile.openstreetmap.jp/styles/osm-bright/style.json",
-        "stylejson/geoservices.un.org/clearmap/style.json",
-        "stylejson/server.arcgisonline.com/world_imagery/style.json",
-        "stylejson/cyberjapandata.gsi.go.jp/xyz/seamlessphoto/style.json",
-      ],
+      options: mapStyleOptions,
       control: { type: "select" },
     },
   },
---
+++ src/components/mapStyleOptions.ts
@@ -0,0 +1,9 @@
+export const mapStyleOptions = [
+  "stylejson/tile.openstreetmap.jp/fiord-color-gl-style/style.json",
+  "stylejson/tile.openstreetmap.fr/hot/style.json",
+  "stylejson/tile.openstreetmap.org/style.json",
+  "https://tile.openstreetmap.jp/styles/osm-bright/style.json",
+  "stylejson/geoservices.un.org/clearmap/style.json",
+  "stylejson/server.arcgisonline.com/world_imagery/style.json",
+  "stylejson/cyberjapandata.gsi.go.jp/xyz/seamlessphoto/style.json",
+];
